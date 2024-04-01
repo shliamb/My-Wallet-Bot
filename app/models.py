cb875fc9582a58359db45be930a8d20a3cbae27a
Shaposhnikov Alexaner <firstremont@gmail.com>
2024-04-02 00:42:51 +0300

    2april24

--- app/models.py
+++ app/models.py
@@ -26,26 +26,26 @@ DATABASE_URL = f"postgresql+asyncpg://{user_db}:{paswor_db}@localhost:5432/my_da
 engine = create_async_engine(DATABASE_URL) # Создание асинхронного движка для работы с базой данных
 Base = declarative_base() # Создание базового класса для объявления моделей
 Column = sqlalchemy.Column
+####
 
 
 class User(Base):
     __tablename__ = 'user_id'
     id = Column(sqlalchemy.BigInteger, primary_key=True, unique=True, nullable=False, index=True) # user id telgram or any data id
     user_name = Column(sqlalchemy.String(50), nullable=True)
-    # is_admin = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False)
-    # is_block = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) 
     currency = Column(sqlalchemy.String(10), default="RUB", server_default="RUB", nullable=False)
+    lang = Column(sqlalchemy.String(50), default="RUS", server_default="RUS", nullable=False)
+    show_balance = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) # show the balance when changing
+    cash = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
     card_1 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
     name_card_1 = Column(sqlalchemy.String(50), nullable=True)
-    # card_2 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    # name_card_2 = Column(sqlalchemy.String(50), nullable=True)
-    # card_3 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    # name_card_3 = Column(sqlalchemy.String(50), nullable=True)
-    # card_4 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    # name_card_4 = Column(sqlalchemy.String(50), nullable=True)
-    # card_5 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    # name_card_5 = Column(sqlalchemy.String(50), nullable=True)
-    cash = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
+    card_2 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
+    name_card_2 = Column(sqlalchemy.String(50), nullable=True)
+    card_3 = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
+    name_card_3 = Column(sqlalchemy.String(50), nullable=True)
+    is_admin = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False)
+    is_block = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False)
+    did_you_donate = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False) # !)
     ####
     sessions = relationship("Sessions", back_populates="users_sessions", uselist=False)
     task = relationship("Task", back_populates="users_task", uselist=False)
@@ -53,12 +53,13 @@ class User(Base):
 class Sessions(Base):
     __tablename__ = 'sessions'
     id = Column(sqlalchemy.BigInteger, ForeignKey('user_id.id'), primary_key=True)
-    date = Column(sqlalchemy.DateTime, nullable=True) 
-    # +-
+    date = Column(sqlalchemy.DateTime, nullable=True)
+    category = Column(sqlalchemy.String(50), default="is no category", server_default="is no category", nullable=False)
+    flow = Column(sqlalchemy.String(5), default="-", server_default="-", nullable=False) # expense/income
     amount = Column(sqlalchemy.Float, default=0, server_default="0", nullable=False)
-    card_number = Column(sqlalchemy.Integer, primary_key=True)
-    # name_card = Column(sqlalchemy.String(50), nullable=True)
     is_cash = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) 
+    card_number = Column(sqlalchemy.Integer, primary_key=True)
+    name_card = Column(sqlalchemy.String(50), nullable=True)
     ####
     users_sessions = relationship("User", back_populates="sessions")
 
@@ -66,9 +67,15 @@ class Task(Base):
     __tablename__ = 'task'
     id = Column(sqlalchemy.BigInteger, ForeignKey('user_id.id'), primary_key=True)
     date = Column(sqlalchemy.DateTime, nullable=True)
+    date_changes = Column(sqlalchemy.DateTime, nullable=True)
     task = Column(sqlalchemy.String(50), nullable=True)
+    is_complite = Column(sqlalchemy.Boolean, default=False, server_default="False", nullable=False) 
     ####
     users_task = relationship("User", back_populates="task")
+
+####
+
+
 
 
 
---
+++ app/worker_db.py
@@ -0,0 +1,150 @@
+from keys import user_db, paswor_db
+import logging
+import asyncio
+import sqlalchemy
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from models import Base, User, Sessions, Task
+from sqlalchemy import select, insert, update, join, func
+
+async def create_async_engine_and_session():                               # localhost
+    engine = create_async_engine(f"postgresql+asyncpg://{user_db}:{paswor_db}@postgres:5432/my_database") # echo=True - вывод логирования
+    async_session = sessionmaker(bind=engine, class_=AsyncSession)
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    return async_session
+
+
+#### USER PROPERTY #### 
+# Read User Data
+async def get_user_by_id(id: int):
+    async_session = await create_async_engine_and_session()
+    async with async_session() as session:
+        # Выполняем запрос на выборку данных пользователя из таблицы User по переданному идентификатору
+        query = select(User).filter(User.id == id)
+        result = await session.execute(query)
+        # Получаем первую строку, которая соответствует запросу
+        data = result.scalar_one_or_none()  # - это метод SQLAlchemy, который возвращает ровно один результат из результата запроса или None, если запрос не вернул ни одного результата.
+        return data or None
+
+# Update User Data
+async def update_user(id: int, updated_data) -> bool: 
+    async_session = await create_async_engine_and_session()
+    confirmation = False
+    async with async_session() as session:
+        try:
+            query = update(User).where(User.id == id).values(**updated_data)
+            await session.execute(query)
+            await session.commit()
+            confirmation = True
+            logging.info(f"User data is update")
+        except Exception as e:
+            logging.error(f"Failed to update user data, error: {e}")
+    return confirmation
+
+# Add User Data to DB
+async def adding_user(user_data) -> bool:
+    async_session = await create_async_engine_and_session()
+    confirmation = False
+    async with async_session() as session:
+        try:
+            query = insert(User).values(**user_data)
+            await session.execute(query)
+            await session.commit()
+            confirmation = True
+            logging.info("User data is add")
+        except Exception as e:
+            logging.error(f"Failed to add user data, error: {e}")
+    return confirmation
+
+
+#### USERS SESSION #### 
+# Read User Session
+async def get_session_by_id(id: int):
+    async_session = await create_async_engine_and_session()
+    async with async_session() as session:
+        query = select(Sessions).filter(Sessions.id == id)
+        result = await session.execute(query)
+        data = result.scalar_one_or_none()
+        return data or None
+
+# # Update User Session - скорее всего не нужно
+# async def update_session(id: int, updated_session) -> bool: 
+#     async_session = await create_async_engine_and_session()
+#     confirmation = False
+#     async with async_session() as session:
+#         try:
+#             query = update(Sessions).where(Sessions.id == id).values(**updated_session)
+#             await session.execute(query)
+#             await session.commit()
+#             confirmation = True
+#             logging.info(f" update session {id}")
+#         except Exception as e:
+#             logging.error(f"Failed to update session: {e}")
+#     return confirmation
+
+# Add User Session to DB
+async def adding_session(session_data) -> bool:
+    async_session = await create_async_engine_and_session()
+    confirmation = False
+    async with async_session() as session:
+        try:
+            query = insert(Sessions).values(**session_data)
+            await session.execute(query)
+            await session.commit()
+            confirmation = True
+            logging.info("Session is add")
+        except Exception as e:
+            logging.error(f"Failed to add session, error: {e}")
+    return confirmation
+
+# All sessions of one user per month/period
+
+# Maybe any think else
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!...
+
+
+
+
+
+#### Task #### 
+# Read Task
+async def get_task_by_id(id: int):
+    async_session = await create_async_engine_and_session()
+    async with async_session() as session:
+        query = select(Task).filter(Task.id == id)
+        result = await session.execute(query)
+        data = result.scalar_one_or_none()
+        return data or None
+
+# Update Task
+async def update_task(id: int, updated_task_data) -> bool: 
+    async_session = await create_async_engine_and_session()
+    confirmation = False
+    async with async_session() as session:
+        try:
+            query = update(Task).where(Task.id == id).values(**updated_task_data)
+            await session.execute(query)
+            await session.commit()
+            confirmation = True
+            logging.info(f"Task is update by user: {id}")
+        except Exception as e:
+            logging.error(f"Failed to update task, error: {e}")
+    return confirmation
+
+# Add Task
+async def adding_task(task_data) -> bool:
+    async_session = await create_async_engine_and_session()
+    confirmation = False
+    async with async_session() as session:
+        try:
+            query = insert(Task).values(**task_data)
+            await session.execute(query)
+            await session.commit()
+            confirmation = True
+            logging.info("Adding one task to DB")
+        except Exception as e:
+            logging.error(f"Failed to add task, errror:: {e}")
+    return confirmation
